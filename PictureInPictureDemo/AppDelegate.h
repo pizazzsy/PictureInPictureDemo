43b05b01d8da7c681a88904b0d9cb806ec362ad7
shiyong <864983345@qq.com>
2020-10-14 15:48:11 +0800

    初始化仓库

---
+++ PictureInPictureDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  PictureInPictureDemo
+//
+//  Created by linkcircle on 2020/10/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ PictureInPictureDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  PictureInPictureDemo
+//
+//  Created by linkcircle on 2020/10/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
